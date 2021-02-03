f1801979a288d0e15084d8077c23f8d7f44bb895
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-03 18:51:18 +0100

    Partially write third test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
     it { should respond_to(:release_bike) }
+    bike = Bike.new
+
+     expect(bike).to eq true
+    #it { should eq(bike) }
+    #it { should respond_to(:working?)}
 end
