0038a624af0c4626182d4c28a9ae0dc4fc4269d0
kjjwhitlock <katiejwhitlock@gmail.com>
2021-02-04 16:51:37 +0000

    mid-question 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,21 @@
 require_relative 'bike'
 
-class DockingStation
 
-    def release_bike
-       Bike.new
-    end
-    
+
+
+class DockingStation
+attr_accessor :bikes
+
+  def initialize
+    @bikes = 0
+  end
+
+  def release_bike
+    Bike.new
+  end
+
+  def dock_bike
+    @bikes += 1
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,7 @@ describe DockingStation do
     it 'should return true if DockingStation.release_bike responds to working?' do
         expect(subject.release_bike).to respond_to (:working?)
     end
+    it 'should dock a bike in the docking station, increasing the number of bikes by 1' do
+        expect { DockingStation.dock_bike}.to change { @bikes }.by(1)
+    end
 end
