560f7228d4a4ac62c16fe2da38ac2d184e491897
jai-jk <jay.jk@hotmail.com>
2021-02-04 16:07:48 +0000

    Write test to check if a new instance of Bike is created and working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
     it { should respond_to(:release_bike) }
-    # bike = Bike.new
-
-     # expect(bike).to eq true
-
-
-    #it { should eq(bike) }
-    #it { should respond_to(:working?)}
+    it 'should return true if DockingStation.release_bike responds to working?' do
+        expect(subject.release_bike).to respond_to (:working?)
+    end
 end
