44045bacae3cb0722acf1e393e113ae7b2668dbb
jai-jk <jay.jk@hotmail.com>
2021-02-05 16:16:10 +0000

    Add tests and functionality to check docking station capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,13 @@ class DockingStation
     attr_reader :bike
 
     def release_bike
+        raise "No bikes available" if !@bike
         Bike.new
     end
 
     def dock(bike)
+        raise "Docking station already full" if @bike
         @bike = bike
     end
-    
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,8 @@ describe DockingStation do
     it { should respond_to(:release_bike) }
 
     it 'should return true if DockingStation.release_bike responds to working?' do
+        bike = Bike.new
+        subject.dock(bike)
         expect(subject.release_bike).to respond_to (:working?)
     end
 
@@ -12,5 +14,15 @@ describe DockingStation do
         subject.dock(bike)
         expect(subject.bike).to eq bike
     end
-    
+
+    it 'should raise an error if no bikes are available' do
+       expect { subject.release_bike }.to raise_error("No bikes available")
+    end
+
+    it 'should raise an error if user attempts to dock_bike at a docking_station that is full' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect { subject.dock(bike) }.to raise_error("Docking station already full")
+    end
+
 end
